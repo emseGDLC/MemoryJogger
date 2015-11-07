c2cbecd4515a235be701b04273e79f06ef2ed63f
Chika Ihejimba <info@chikaihejimba.com>
2015-11-07 13:39:08 -0600

    added request popup

--- MemoryJogger/MemoryJogger/www/js/controllers.js
+++ MemoryJogger/MemoryJogger/www/js/controllers.js
@@ -116,4 +116,43 @@ angular.module('starter.controllers', [])
                
        };
    })
+.controller('showRequestPopupCtrl', [
+                    '$scope', '$ionicModal',
+                    function ($scope, $ionicModal) {
+                        $ionicModal.fromTemplateUrl('templates/requestPopup.html', {
+                            scope: $scope,
+                            animation: 'slide-in-up'
+                        }).then(function (modal) {
+                            $scope.modal = modal;
+                        });
+
+                        $scope.openModal = function () {
+                            $scope.modal.show();
+                        };
+
+                        $scope.closeModal = function () {
+                            $scope.modal.hide();
+                        };
+
+                        //Cleanup the modal when we're done with it!
+                        $scope.$on('$destroy', function () {
+                            $scope.modal.remove();
+                        });
+                        // Execute action on hide modal
+                        $scope.$on('modal.hide', function () {
+                            // Execute action
+                        });
+                        // Execute action on remove modal
+                        $scope.$on('modal.removed', function () {
+                            // Execute action
+                        });
+                        $scope.$on('modal.shown', function () {
+                            console.log('Modal is shown!');
+                        });
+
+                        $scope.showRequest = function (index) {
+                            $scope.openModal();
+                        }
+                    }
+])
 ;
